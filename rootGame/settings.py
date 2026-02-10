f04491fe3b8ef8fa4e614968cfe3c9674aede60d
mdfeinstein <110838263+mdfeinstein@users.noreply.github.com>
2026-02-10 17:01:42 -0500

    cloudflare quick tunnel
    new domain each restart of the container...
    need to buy a domain for something permanent

--- rootGame/settings.py
+++ rootGame/settings.py
@@ -34,7 +34,6 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost 127.0.0.1").split(" ")
 
-
 # Application definition
 
 INSTALLED_APPS = [
