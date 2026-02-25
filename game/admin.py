c38dafbb3ccd296a56a64930d3d8f11776686c26
mdfeinstein <110838263+mdfeinstein@users.noreply.github.com>
2026-02-25 10:59:30 -0500

    added admin capabilities to delete games

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models.game_models import Game
 
-# Register your models here.
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'status', 'boardmap')
